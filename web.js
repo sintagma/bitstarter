3a0712dd773d2b1fe8c966a14946045b418e6a94
Ubuntu <ubuntu@ip-172-31-15-219.us-west-2.compute.internal>
2013-07-14 13:38:49 +0000

    Added var buf in web.js

--- web.js
+++ web.js
@@ -4,8 +4,10 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
+var buf = new Buffer();
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html'));
+  response.send(buf.toString('utf8', 0, fs.readFileSync('index.html')));
 });
 
 var port = process.env.PORT || 5000;
