a26b9d81cda5b244fb5c06d4d4308bf7c51507f6
Ubuntu <ubuntu@ip-172-31-15-219.us-west-2.compute.internal>
2013-07-14 21:29:41 +0000

    Added restler library

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var restler = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
