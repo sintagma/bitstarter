bf49848a50344d65aa12dd27f1a4987b905f8d99
sintagma <sintagma@yahoo.com>
2013-08-27 06:56:42 +0000

    Changed web server port to 8080

--- web.js
+++ web.js
@@ -15,7 +15,6 @@ app.get('/', function(request, response) {
   response.send(text);
  });
 
-
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
